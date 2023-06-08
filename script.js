ef60270e7f58ba3a65ffdd21efcd5b53dd09b816
Gaëlle Viso <gaelleviso@gmail.com>
2023-06-08 16:10:59 +0200

    Calculator completely functional but can't operate on multiple digit numbers

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let display = document.getElementById("display");
 let warning = document.getElementById("warning");
 let screenText = display.textContent
 let message = ''
+let secondOperator = false
 
 buttons.forEach((button) => {
     let buttonText = button.textContent;
@@ -13,10 +14,16 @@ buttons.forEach((button) => {
     button.addEventListener('click', () => buttonClicked(buttonText, cla))});
 
 
-function toDisplayOrNotToDisplay (buttonText, cla) {
+function toDisplayOrNotToDisplay (buttonText, cla, onlyDisplayed) {
     if (!cla.includes('nodisplay')){
-        display.textContent = buttonText
-        screenText = display.textContent
+        if (onlyDisplayed) {
+            display.textContent = buttonText
+        }
+        else {
+            display.textContent += buttonText
+        }
+            screenText = display.textContent
+        
     }
 }
 
@@ -24,25 +31,31 @@ function warn (message) {
     warning.textContent = message
 }
 
+function dontWarn () {
+    warn('')
+}
+
 function buttonClicked(buttonText, cla) {
         if (cla.includes('number')){
             if (firstNumber == null) {
                     firstNumber = buttonText
-                    toDisplayOrNotToDisplay(buttonText, cla)
+                    toDisplayOrNotToDisplay(buttonText, cla, true)
+                    dontWarn()
             }
             else if (secondNumber == null) {
                 if (!(operator == null)) {
                     secondNumber = buttonText
-                    toDisplayOrNotToDisplay(buttonText, cla)
+                    toDisplayOrNotToDisplay(buttonText, cla, false)
+                    dontWarn()
                 }
                 else {
                     warn('Please pick the operator before picking the second number')
-                    console.log(operator)
                 }
             }
             else {
                 warn('You have picked both numbers already')
             }
+            secondOperator = false
         }
         if (cla.includes('operator')){
             if (operator == null) {
@@ -51,17 +64,76 @@ function buttonClicked(buttonText, cla) {
                 }
                 else {
                     operator = buttonText
-                    toDisplayOrNotToDisplay(buttonText, cla)
+                    toDisplayOrNotToDisplay(buttonText, cla, false)
+                    dontWarn()
                 }
+            }
+            if (!(firstNumber == null) && !(secondNumber == null) && !(operator == null)) {
+                firstNumber = operate(firstNumber, secondNumber, operator)
+                display.textContent = firstNumber
+                operator = buttonText
+                toDisplayOrNotToDisplay(buttonText, cla, false)
+                secondOperator = true
             }
         }
         if (cla.includes('equal')){
             if (!(firstNumber == null) && !(secondNumber == null) && !(operator == null)) {
-                display.textContent = operate(firstNumber, secondNumber, operator)
+                firstNumber = operate(firstNumber, secondNumber, operator)
+                display.textContent = firstNumber
             }
             else {
                 warn('Please pick two numbers and an operator before proceding')
             } 
+        }
+        if (cla.includes('AC')) {
+            firstNumber = null
+            secondNumber = null
+            operator = null
+            display.textContent = '0'
+            dontWarn()
+        }
+        if (cla.includes('justC')) {
+            dontWarn()
+            if (secondOperator) {
+                display.textContent = firstNumber
+                operator = null
+            }
+            else if (!(secondNumber == null)) {
+                display.textContent = firstNumber + operator
+                secondNumber = null
+            }
+            else if (!(operator == null)) {
+                display.textContent = firstNumber
+                operator = null
+            }
+            else {
+                display.textContent = '0'
+                firstNumber = null
+            }
+        }
+        if (cla.includes('percent')) {
+            display.textContent = display.textContent / 100 
+            firstNumber = Math.round(display.textContent * 10000) / 10000
+            display.textContent = firstNumber
+            secondNumber = null
+            operator = null
+            dontWarn()
+        }
+        if (cla.includes('signchange')) {
+            if (!(secondNumber == null)) {
+                secondNumber *= '-1'
+                if (secondNumber < 0) {
+                    display.textContent = firstNumber + operator + '(' + secondNumber + ')'
+                }
+                else {
+                    display.textContent = firstNumber + operator + secondNumber
+                }
+            }
+            else {
+                firstNumber *= '-1'
+                display.textContent = firstNumber
+
+            }
         }
 }
 
@@ -81,19 +153,25 @@ function divide(a, b) {
     return parseFloat(a) / parseFloat(b)
 }
 
-function operate(firstNumber, secondNumber, operator) {
-    if (operator == '+') {
-        return add(firstNumber, secondNumber)
+function operate(a, b, op) {
+    let answer
+    if (op == '+') {
+        answer = add(a, b)
     }
-    if (operator == '-') {
-        return subtract(firstNumber, secondNumber)
+    if (op == '-') {
+        answer =  subtract(a, b)
     }
-    if (operator == '×') {
-        return multiply(firstNumber, secondNumber)
+    if (op == '×') {
+        answer = multiply(a, b)
     }
-    if (operator == '÷') {
-        return divide(firstNumber, secondNumber)
+    if (op == '÷') {
+        answer = divide(a, b)
     }
+    secondNumber = null
+    operator = null
+    dontWarn()
+    answer = Math.round(answer * 10000) / 10000
+    return answer
 }
 
 // TODO Pick multiple digit numbers
