6d11b756b52307aa53ba2e4218472d2eaaa2b82f
Gaëlle Viso <gaelleviso@gmail.com>
2023-06-08 14:16:12 +0200

    Merges all js to one file and created variable for what is written on screen

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ let firstNumber
 let secondNumber
 let operator
 
+// const equal = document.getElementById('equal');
+// equal.addEventListener('click', function() {console.log(display.textContent)})
+
+let buttons = document.querySelectorAll(".btn");
+let display = document.getElementById("display");
+let screenText
+
+buttons.forEach((button) => {
+    let buttonText = button.textContent;
+    let cla = button.getAttribute('class');
+    button.addEventListener('click', () => buttonClicked(buttonText, cla))});
+
+function buttonClicked(buttonText, cla) {
+        if (!cla.includes('nodisplay')){
+            display.textContent = buttonText
+            screenText = display.textContent
+        }
+}
+
 function add(a, b) {
     return a + b
 }
